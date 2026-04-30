068637527a66ade49255b573491d1459d22c89b6
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-30 12:55:42 +0200

    adds initialize optimizer method

--- pqn/PQN.py
+++ pqn/PQN.py
@@ -8,14 +8,21 @@ class PQN:
         return_lambda: float = 0.65,
         gamma: float = 0.99,
         optimizer: PQNOptimizerType = "radam",
+        optimizer_first_beta: float = 0.99,
+        optimizer_second_beta: float = 0.999,
+        optimizer_epsilon: float = 1e-5,
     ):
         params = locals()
         params.pop("self")
         self.__initialize_hyper_parameters(params)
+        self.__initialize_optimizer()
 
     def __initialize_hyper_parameters(self, params: dict):
         for key, value in params:
             setattr(self, key, value)
+
+    def __initialize_optimizer(self):
+        pass
 
     def train(self, *, environment: str, seed: int):
         pass
