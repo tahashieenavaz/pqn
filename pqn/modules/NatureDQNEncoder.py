9ac320ca04a1f8dc2243153d78c0c4731b3f7aaa
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-01 17:04:24 +0200

    adds nature dqn encoder

---
+++ pqn/modules/NatureDQNEncoder.py
@@ -0,0 +1,24 @@
+import torch
+from typing import Type
+from torch.nn import Module
+from .LayerNorm2d import LayerNorm2d
+
+
+class NatureDQNEncoder(torch.nn.Module):
+    def __init__(self, *, activation: Type[Module] = torch.nn.ReLU):
+        super().__init__()
+        self.stream = torch.nn.Sequential(
+            torch.nn.Conv2d(4, 32, kernel_size=8, stride=4, padding=0, bias=False),
+            LayerNorm2d(32),
+            activation(inplace=True),
+            torch.nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0, bias=False),
+            LayerNorm2d(64),
+            activation(inplace=True),
+            torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0, bias=False),
+            LayerNorm2d(64),
+            activation(inplace=True),
+            torch.nn.Flatten(),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.stream(x)
