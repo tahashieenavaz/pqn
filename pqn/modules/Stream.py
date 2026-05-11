e3b06442b057e846be761ff74297e726d1ce889e
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-11 15:02:20 +0200

    fixes stream conduct

--- pqn/modules/Stream.py
+++ pqn/modules/Stream.py
@@ -8,15 +8,21 @@ class Stream(torch.nn.Module):
         input_dimension: int,
         hidden_dimension: int,
         output_dimension: int,
+        normalization: bool,
         activation: Type[torch.nn.Module] = torch.nn.ReLU,
     ):
         super().__init__()
-        self.stream = torch.nn.Sequential(
-            torch.nn.Linear(input_dimension, hidden_dimension),
-            torch.nn.LayerNorm(hidden_dimension),
-            activation(inplace=True),
-            torch.nn.Linear(hidden_dimension, output_dimension),
-        )
+        self.normalization = normalization
+        self.first_linear = torch.nn.Linear(input_dimension, hidden_dimension)
+        self.second_linear = torch.nn.Linear(hidden_dimension, output_dimension)
+        self.activation = activation()
+        if normalization:
+            self.normalization_layer = torch.nn.LayerNorm(hidden_dimension)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.stream(x)
+        x = self.first_linear(x)
+        if self.normalization:
+            x = self.normalization_layer(x)
+        x = self.activation(x)
+        x = self.second_linear(x)
+        return x
